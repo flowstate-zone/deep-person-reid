ce0ef08b9f05ae068619a282f82d9f05d81b3570
Chris Hausler <chris@flowstate.zone>
2023-08-29 21:31:27 +0000

    flowstate eval

--- torchreid/metrics/rank.py
+++ torchreid/metrics/rank.py
@@ -153,12 +153,78 @@ def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
     return all_cmc, mAP
 
 
+def eval_flowstate(distmat, q_pids, g_pids, q_camids, g_camids, q_dsetids, g_dsetids, max_rank):
+    """Evaluation with market1501 metric
+    Key: for each query identity, its gallery images from the same camera view are discarded.
+    """
+    num_q, num_g = distmat.shape
+
+    if num_g < max_rank:
+        max_rank = num_g
+        print(
+            'Note: number of gallery samples is quite small, got {}'.
+            format(num_g)
+        )
+
+    indices = np.argsort(distmat, axis=1)
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+
+    # compute cmc curve for each query
+    all_cmc = []
+    all_AP = []
+    num_valid_q = 0. # number of valid query
+
+    for q_idx in range(num_q):
+        # get query pid and camid
+        q_pid = q_pids[q_idx]
+        q_camid = q_camids[q_idx]
+        q_dsetid = q_dsetids[q_idx]
+
+        # remove gallery samples that have the same pid and camid with query or are from a different dataset
+        order = indices[q_idx]
+        remove = ((g_pids[order] == q_pid) & (g_camids[order] == q_camid)) | (g_dsetids[order] != q_dsetid)
+        keep = np.invert(remove)
+
+        # compute cmc curve
+        raw_cmc = matches[q_idx][
+            keep] # binary vector, positions with value 1 are correct matches
+        if not np.any(raw_cmc):
+            # this condition is true when query identity does not appear in gallery
+            continue
+
+        cmc = raw_cmc.cumsum()
+        cmc[cmc > 1] = 1
+
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        num_rel = raw_cmc.sum()
+        tmp_cmc = raw_cmc.cumsum()
+        tmp_cmc = [x / (i+1.) for i, x in enumerate(tmp_cmc)]
+        tmp_cmc = np.asarray(tmp_cmc) * raw_cmc
+        AP = tmp_cmc.sum() / num_rel
+        all_AP.append(AP)
+
+    assert num_valid_q > 0, 'Error: all query identities do not appear in gallery'
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mAP = np.mean(all_AP)
+
+    return all_cmc, mAP
+
 def evaluate_py(
-    distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03
+    distmat, q_pids, g_pids, q_camids, g_camids, q_dsetids, gdsetids, max_rank, use_metric_cuhk03, use_flowstate
 ):
     if use_metric_cuhk03:
         return eval_cuhk03(
             distmat, q_pids, g_pids, q_camids, g_camids, max_rank
+        )
+    elif use_flowstate:
+        return eval_flowstate(
+            distmat, q_pids, g_pids, q_camids, q_dsetids, gdsetids, g_camids, max_rank
         )
     else:
         return eval_market1501(
@@ -172,9 +238,12 @@ def evaluate_rank(
     g_pids,
     q_camids,
     g_camids,
+    q_dsetids=None,
+    g_dsetids=None,
     max_rank=50,
     use_metric_cuhk03=False,
-    use_cython=True
+    use_cython=True,
+    use_flowstate=False
 ):
     """Evaluates CMC rank.
 
@@ -195,6 +264,12 @@ def evaluate_rank(
             This is highly recommended as the cython code can speed up the cmc computation
             by more than 10x. This requires Cython to be installed.
     """
+    if use_flowstate:
+        return evaluate_py(
+            distmat, q_pids, g_pids, q_camids, g_camids, max_rank,
+            use_metric_cuhk03, use_flowstate
+        )
+
     if use_cython and IS_CYTHON_AVAI:
         return evaluate_cy(
             distmat, q_pids, g_pids, q_camids, g_camids, max_rank,
@@ -203,5 +278,5 @@ def evaluate_rank(
     else:
         return evaluate_py(
             distmat, q_pids, g_pids, q_camids, g_camids, max_rank,
-            use_metric_cuhk03
+            use_metric_cuhk03, use_flowstate
         )
